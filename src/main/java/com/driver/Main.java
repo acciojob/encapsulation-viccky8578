a62279bf96b49c8716604fa3cf8aefa28838323c
viccky8578 <manbodhvikram@gmail.com>
2023-02-10 13:19:47 +0530

    code of encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args)
+    {
+        RWOnly rw=new RWOnly();
+       // rw.name("vikram");
+        rw.setName("vikram");
+      // System.out.println( rw.getName());
+        rw.getName();
+
+    }
   
 }
